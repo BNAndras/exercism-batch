e4cd2350b50a04210315c378518ad4e2cf23ddd6
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-01-19 17:03:24 -0800

    Update `nth-prime` example

--- exercises/practice/nth-prime/.meta/Example.bat
+++ exercises/practice/nth-prime/.meta/Example.bat
@@ -1,44 +1,54 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set targetNTH=%~1
-set /a "currentNTH=0"
-set "primeNumbers="
+set "targetNTH=%~1"
+set /a "currentCount=0"
 
-if %targetNTH% equ 0 (
-  echo there is no zeroth prime
-  exit /b 0
+if %targetNTH% LEQ 0 (
+    echo there is no zeroth prime
+    exit /b 0
 )
 
-for /L %%a in (1,1,10001) do (
-  set index=%%a
-  2>nul set /a "1/(!index! %% 2)" && (
-    if !index! neq 1 (
-      set divideableCheck=true
-      for %%b in (!primeNumbers!) do (
-        set /a "remainder=!index! %% %%b"
-        if !remainder! equ 0 (
-          set divideableCheck=false
-        )
-      )
-
-      if !divideableCheck! equ true (
-        set primeNumbers=!primeNumbers! !index!
-        set /a "currentNTH+=1"
-        if !currentNTH! equ %targetNTH% (
-          echo !index!
-          exit /b 0
-        )
-      )
-    )
-  )
-
-  if !index! equ 2 (
-    set primeNumbers=!primeNumbers! !index!
-    set /a "currentNTH+=1"
-    if !currentNTH! equ %targetNTH% (
-      echo !index!
-      exit /b 0
-    )
-  )
+rem Case 1: First prime is 2
+if %targetNTH% EQU 1 (
+    echo 2
+    exit /b 0
 )
+
+set /a "currentCount=1"
+set "candidate=1"
+
+:FindNextPrime
+    set /a "candidate+=2"
+    
+    rem Check if candidate is prime
+    set "isPrime=1"
+    
+    rem Only check up to sqrt(candidate). We iterate odd numbers.
+    rem We can approximate loop limit or just check all odd numbers < candidate/2
+    rem Optimally: for j=3; j*j <= candidate; j+=2
+    
+    set /a "j=3"
+    
+    :CheckDivisor
+        set /a "sq=j*j"
+        if %sq% GTR %candidate% goto :PrimeFound
+        
+        set /a "rem=candidate %% j"
+        if %rem% EQU 0 (
+            set "isPrime=0"
+            goto :NotPrime
+        )
+        
+        set /a "j+=2"
+        goto :CheckDivisor
+        
+    :PrimeFound
+    set /a "currentCount+=1"
+    if %currentCount% EQU %targetNTH% (
+        echo %candidate%
+        exit /b 0
+    )
+    
+    :NotPrime
+    goto :FindNextPrime
